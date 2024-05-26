4da1ec54b0c6da08179ed88391582e15a337578a
GAURAV SHARMA <108004723+gauravrdj@users.noreply.github.com>
2024-05-26 15:30:03 +0530

    first commit

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -6,8 +6,19 @@ import waveSVG from '../assets/svg/101786-wave.json';
 import emailSVG from '../assets/svg/95247-email.json';
 import viteSVG from '../assets/svg/viteLogo.svg';
 import netlifySVG from '../assets/svg/netlifyLogo.svg';
+import tsLogoSVG from '../assets/svg/tsLogo.svg';
+import node from '../assets/svg/Node.js_logo.svg';
+import next from '../assets/svg/next.svg';
+import git from '../assets/svg/git.svg';
+import clodflare from '../assets/svg/cloudflare.svg';
+import mongo from '../assets/svg/mongo.svg';
+import postgres from '../assets/svg/postgres.svg';
+import turbo from '../assets/svg/turbo.svg';
+import cpp from '../assets/svg/cpp.svg';
+import tailwind from '../assets/svg/tailwind.svg';
+import express from '../assets/svg/express.svg';
 import sassSVG from '../assets/svg/sassLogo.svg';
-import myResume from '../assets/pdf/ocean-waring-resume.pdf';
+import myResume from '../assets/pdf/GauravResume_offCampus.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub, faHtml5, faCss3, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
@@ -29,8 +40,9 @@ export default function HomePage() {
               className="logo"
               ref={ref}
               onClick={() => ref.current.scrollTo(0)}
+              style={{color:"purple"}}
             >
-              OCEAN
+              गौरव शर्मा
               <span
                 style={{
                   color: "rgb(217, 4, 41)",
@@ -50,16 +62,18 @@ export default function HomePage() {
                     ? ref.current.scrollTo(2.5)
                     : ref.current.scrollTo(2.25)
                 }
+                style={{color:"purple"}}
               >
                 PROJECTS
               </li>
-              <li ref={ref} onClick={() => ref.current.scrollTo(1)}>
+              <li ref={ref} onClick={() => ref.current.scrollTo(1)} style={{color:"purple"}}>
                 RESUME
               </li>
               <li
                 style={{ fontWeight: "200" }}
                 ref={ref}
                 onClick={() => ref.current.scrollTo(3)}
+                style={{color:"purple"}}
               >
                 CONTACT
               </li>
@@ -71,23 +85,24 @@ export default function HomePage() {
           <section className="introSection">
             <div style={{ display: "flex" }}>
               <div className="introText">
-                <h1>MY NAME IS OCEAN WARING</h1>
-                <p>FRONTEND REACT DEVELOPER</p>
-                <p>
-                  Based in NYC{" "}
+                <h1 style={{color:"purple"}}>I AM GAURAV SHARMA</h1>
+                <h2>Electrical Engineering | NIT RKL'25</h2>
+                <h2>MERN Developer</h2>
+                <h2>
+                  Based in Jaipur{" "}
                   <FontAwesomeIcon
                     icon={faLocationDot}
                     style={{ color: "red" }}
                   />
-                </p>
+                </h2>
                 <div className="contactBtns">
                   <a
                     target="#"
-                    href="https://www.linkedin.com/in/ocean-waring/"
+                    href="www.linkedin.com/in/gaurav-sharma-9556b2196"
                   >
                     <FontAwesomeIcon className="linkedin" icon={faLinkedin} />
                   </a>
-                  <a target="#" href="https://github.com/Ocn-W">
+                  <a target="#" href="https://github.com/gauravrdj">
                     <FontAwesomeIcon className="github" icon={faGithub} />
                   </a>
                 </div>
@@ -97,23 +112,16 @@ export default function HomePage() {
           </section>
         </ParallaxLayer>
 {/* SVG Transition Section */}
-        <ParallaxLayer offset={0.75} speed={1}>
+        {/* <ParallaxLayer offset={0.75} speed={1}>
           <Lottie className="lottieWave" animationData={waveSVG} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 {/* About Me Section */}
         <ParallaxLayer offset={1}>
           <section className="aboutMe">
             <div className="aboutIntro">
-              <h2>ABOUT ME</h2>
+              <h2 style={{color:"purple"}}>ABOUT ME</h2>
               <p>
-                Hello! I am a Front End React developer based in NYC.
-                Currently building a strong and growing skill set in HTML5, CSS3/SCSS,
-                JavaScript, Redux/Jotai, React, React Native, and TDD using Jest. I consistently improve a keen eye for design
-                and a commitment to producing logical, accessible, and
-                responsive code. I am dedicated to continuous learning and
-                staying up-to-date with the latest trends and best coding
-                practices, ensuring that I can be a useful team player and
-                provide efficient solutions. Let's connect and discuss how I can
+              Welcome to my portfolio! I am Gaurav Sharma, an aspiring Electrical Engineer currently in my prefinal year at NIT Rourkela. Alongside my core engineering studies, I am a passionate MERN stack developer with a strong foundation in C++ and Data Structures & Algorithms (DSA).  Let's connect and discuss how I can
                 contribute to your web development team!
               </p>
             </div>
@@ -128,7 +136,7 @@ export default function HomePage() {
             </div>
           </section>
           <section className="techStack">
-            <h2>MY TECHSTACK</h2>
+            <h2 style={{color:"purple"}}>MY TECHSTACK</h2>
             <div className="scrollStack">
               <div className="stackLogos">
                 <FontAwesomeIcon
@@ -146,14 +154,26 @@ export default function HomePage() {
                   icon={faJsSquare}
                   style={{ color: "gold" }}
                 />
-                <img src={sassSVG} />
+                {/* <img src={sassSVG} /> */}
+                
                 <FontAwesomeIcon
                   className="reactLogo"
                   icon={faReact}
                   style={{ color: "cyan" }}
                 />
                 <img src={viteSVG} />
+                <img src={cpp} />
                 <img src={netlifySVG} />
+                <img src={tsLogoSVG} />
+                <img src={node} />
+                <img src={next} />
+                <img src={git} />
+                <img src={clodflare} />
+                <img src={mongo} />
+                <img src={turbo} />
+                <img src={postgres} />
+                <img src={express} />
+                <img src={tailwind} />
               </div>
               <div className="stackLogos" aria-hidden="true">
                 <FontAwesomeIcon
@@ -171,14 +191,26 @@ export default function HomePage() {
                   icon={faJsSquare}
                   style={{ color: "gold" }}
                 />
-                <img src={sassSVG} />
+                {/* <img src={sassSVG} /> */}
+                <FontAwesomeIcon icon="fa-brands fa-node-js" />
                 <FontAwesomeIcon
                   className="reactLogo"
                   icon={faReact}
                   style={{ color: "cyan" }}
                 />
                 <img src={viteSVG} />
-                <img src={netlifySVG} /> 
+                <img src={cpp} />
+                <img src={netlifySVG} />
+                <img src={tsLogoSVG} />
+                <img src={node} />
+                <img src={next} />
+                <img src={git} />
+                <img src={clodflare} />
+                <img src={mongo} />
+                <img src={turbo} />
+                <img src={postgres} /> 
+                <img src={express} />
+                <img src={tailwind} />
               </div>
             </div>
           </section>
@@ -186,7 +218,7 @@ export default function HomePage() {
 {/* Projects Section */}
         <ParallaxLayer offset={window.innerWidth < 768 ? 2.5 : 2.25}>
           <section className="projectSection">
-            <h2>PROJECTS</h2>
+            <h2 style={{color:"purple"}}>PROJECTS</h2>
             <Projects />
           </section>
         </ParallaxLayer>
@@ -198,7 +230,7 @@ export default function HomePage() {
         >
           <Lottie className="lottieEmail" animationData={emailSVG} />
           <section className="contactSection">
-            <h2>LETS WORK</h2>
+            <h2 style={{color:"purple"}}>LETS WORK</h2>
             <div className="formContainer">
               <form
                 netlify
@@ -217,6 +249,7 @@ export default function HomePage() {
                     name="name"
                     required
                     aria-required="true"
+                    placeholder='Gaurav Sharma'
                   />
                 </label>
 
@@ -228,6 +261,7 @@ export default function HomePage() {
                     name="email"
                     required
                     aria-required="true"
+                    placeholder='gauravjdh2021@gmail.com'
                   />
                 </label>
 
@@ -238,10 +272,11 @@ export default function HomePage() {
                     rows="4"
                     required
                     aria-required="true"
+                    placeholder='Hi There!'
                   ></textarea>
                 </label>
 
-                <button type="submit">Submit</button>
+                <button type="submit" >Submit</button>
               </form>
             </div>
           </section>
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -6,11 +6,23 @@ import netlifySVG from '../assets/svg/netlifyLogo.svg';
 import '../css/Projects.css';
 import sassSVG from '../assets/svg/sassLogo.svg';
 import webGenerator from '../assets/WebsiteGenerator.jpg';
-import playlistMaker from '../assets/PlaylistMaker.jpg';
-import retailSite from '../assets/RetailSite.jpg';
-import qrGenSite from '../assets/QRGeneratorSite.jpg';
+import profile from '../assets/profile.png';
+import paymentsApp from '../assets/paymentsApp.jpeg';
+import medium from '../assets/medium.jpeg';
 import authySite from '../assets/TicketAuthSite.jpg';
 import { faArrowLeft, faArrowRight, faCircle } from '@fortawesome/free-solid-svg-icons';
+import tsLogoSVG from '../assets/svg/tsLogo.svg';
+import node from '../assets/svg/Node.js_logo.svg';
+import next from '../assets/svg/next.svg';
+import git from '../assets/svg/git.svg';
+import clodflare from '../assets/svg/cloudflare.svg';
+import mongo from '../assets/svg/mongo.svg';
+import postgres from '../assets/svg/postgres.svg';
+import turbo from '../assets/svg/turbo.svg';
+import cpp from '../assets/svg/cpp.svg';
+import tailwind from '../assets/svg/tailwind.svg';
+import express from '../assets/svg/express.svg';
+import prisma from '../assets/svg/prisma.svg';
 
 
 
@@ -23,6 +35,18 @@ export default function Projects() {
   const netlifyLogo = <img src={netlifySVG} className='netlifyLogo' alt="Netlify Logo"/>;
   const viteLogo = <img src={viteSVG} className='viteLogo' alt="Vite Logo"/>;
   const sassLogo = <img src={sassSVG} className='sassLogo' alt='Sass Logo'/>;
+  const tsLogo = <img src={tsLogoSVG} className='viteLogo'></img>
+  const nodeLogo = <img src={node} className='viteLogo'></img>
+  const nextLogo = <img src={next} className='viteLogo'></img>
+  const gitLogo = <img src={git} className='viteLogo'></img>
+  const cloudflareLogo = <img src={clodflare} className='viteLogo'></img>
+  const mongoLogo = <img src={mongo} className='viteLogo'></img>
+  const postgresLogo = <img src={postgres} className='viteLogo'></img>
+  const turboLogo = <img src={turbo} className='viteLogo'></img>
+  const cppLogo = <img src={cpp} className='viteLogo'></img>
+  const tailwindLogo = <img src={tailwind} className='viteLogo'></img>
+  const expressLogo = <img src={express} className='viteLogo'></img>
+  const prismaLogo = <img src={prisma} className='viteLogo'></img>
 
   function updateIndex(newIndex) {
     newIndex < 0
@@ -35,171 +59,198 @@ export default function Projects() {
 
   const projects = [
     {
-      name: "Retail Website",
+      name: "Payments App",
       techstack: (
         <>
           {viteLogo}
           {reactLogo}
           {jsLogo}
-          {sassLogo}
+          {nodeLogo}
+          {expressLogo}
+          {tailwindLogo}
+          {mongoLogo}
         </>
       ),
       dependencies: (
         <>
-          {netlifyLogo}
-          <p>Jotai</p>
+          
+          <p>Mongoose Sessions</p>
           <p>React Router</p>
+          <p>Zod</p>
+          <p>JWT</p>
         </>
       ),
       description: (
         <>
-          <p>
-            This is a mock e-commerce website using front-end features combined
-            with <em>global state management handled by Jotai</em> and{" "}
-            <em>pagination with React Router</em>. Features include a randomly
-            generated list of clothing items to select your desired purchase,
-            filtering clothing items on the clothing page by price or rating,
-            adding items from your shopping or favorites cart, and a checkout
-            page to view your total and mock form to complete purchases.
-          </p>
+          <ul>
+            <li>
+            Designed and implemented a fully secure payment application facilitating user-to-user money transfers.
+            </li>
+            <li>
+            Prioritized user safety by employing industry standard security protocols and addressing real-time transaction
+ success and failure scenarios.
+            </li>
+            <li>
+            Leveraged Mongoose sessions for efficient transaction tracking and stored user data securely in MongoDB.
+            </li>
+            <li>
+            Crafted a scalable and performant backend utilizing Express.js and Node.js and a frontend using React and
+ Tailwind.
+            </li>
+          </ul>
         </>
       ),
-      demoUrl: "https://voguevalue.netlify.app",
-      image: retailSite,
+      demoUrl: "https://securetransaction.netlify.app/",
+      image: paymentsApp,
     },
     {
-      name: "QR Code Generator",
+      name: "Medium: Publishing platform",
       techstack: (
         <>
           {viteLogo}
           {reactLogo}
-          {jsLogo}
+          {tsLogo}
+          {postgresLogo}
+          {prismaLogo}
+          {cloudflareLogo}
         </>
       ),
       dependencies: (
         <>
-          {netlifyLogo}
-          <p>react-qr-code</p>
+          
+          
+          <p>Hono</p>
+          <p>ServerLess Backends</p>
+          <p>Prisma - ORM</p>
+          <p>JWT</p>
+          <p>Zod</p>
         </>
       ),
       description: (
         <>
-          <p>
-            This website makes use of the library react-qr-code to generate,
-            customize, and download your own QR Codes for websites, business
-            cards, social media, you name it! This project highlights some
-            interesting features possible with the library such as{" "}
-            <em>SVG to PNG conversion for file downloads</em> and{" "}
-            <em>state management to handle the styling of SVG elements</em>.
-          </p>
+          <ul>
+            <li>
+            Developed a Medium-like publishing web application using TypeScript and Hono on the backend, leveraging
+ Cloudflare for enhanced security and performance.
+            </li>
+            <li>
+            Implemented a robust frontend using React and Tailwind CSS, focusing on user experience and modern design
+ principles.
+            </li>
+            <li>
+            Utilized PostgreSQL as the database management system, integrated seamlessly with Prisma ORM for efficient
+ data handling and manipulation.
+            </li>
+            <li>
+            Implemented input validation using Zod and secured user authentication with JWT, ensuring data integrity and
+ user privacy throughout the application.
+            </li>
+          </ul>
         </>
       ),
-      demoUrl: "https://qrbox.netlify.app/",
-      image: qrGenSite,
+      demoUrl: "https://publishingmedium.netlify.app/",
+      image: medium,
     },
     {
-      name: "Spotify Playlist Maker",
+      name: " Dynamic Profile Management System",
       techstack: (
         <>
+          {/* {viteLogo} */}
           {reactLogo}
           {jsLogo}
+          {nodeLogo}
+          {expressLogo}
+          {tailwindLogo}
+          {mongoLogo}
         </>
       ),
       dependencies: (
         <>
-          {netlifyLogo}
-          <p>React-Context</p>
-          <p>Spotify API</p>
+          <p>Dynamic Routing</p>
+          <p>React-Router-Dom</p>
         </>
       ),
       description: (
         <>
-          <p>
-            This web application makes use of the SpotifyAPI to search your
-            favorite songs and create custom playlists to upload directly to
-            Spotify. The major features for this website include searching the
-            Spotify database to find your favorite songs and creating/editing
-            custom playlist to publish to your Spotify account. This project
-            served as my way to dive into understanding core concepts around{" "}
-            <em>React Context</em>,{" "}
-            <em>handling token requests and API data</em>,{" "}
-            <em>
-              hiding important API keys for production with dotenv and through
-              Netlify
-            </em>
-            , <em>repository history management on GitHub with GitBash</em>, as
-            well as the overall workflow for SPA's in React without using major
-            libraries.{" "}
-            <strong>
-              <em>
-                You do not need to authorize your account to create the
-                playlist, only to upload!
-              </em>
-            </strong>
-          </p>
+          <ul>
+            <li>
+            Created a web application allowing users to manage multiple profiles (e.g., LinkedIn, GitHub, Facebook) by
+ dynamically adding profile links.
+            </li>
+            <li>
+            Implemented using React for the front end, Node.js with Express.js for the back end, and MongoDB for data
+ management.
+            </li>
+
+            <li>
+            Users can access their profiles just by using their name and profile name. 
+            </li>
+            <br />
+            <li> Ex.- <a href="https://getmyprofile.netlify.app/gaurav/leetcode">/gaurav/leetcode</a></li>
+          </ul>
         </>
       ),
-      demoUrl: "https://soundstack.netlify.app",
-      image: playlistMaker,
+      demoUrl: "https://getmyprofile.netlify.app/",
+      image: profile,
     },
-    {
-      name: "Ticket Authentication Site",
-      techstack: (
-        <>
-          {viteLogo}
-          {reactLogo}
-          {jsLogo}
-          {sassLogo}
-        </>
-      ),
-      dependencies: (
-        <>
-          {netlifyLogo}
-          <p>React Context</p>
-        </>
-      ),
-      description: (
-        <>
-          <p>
-            This website explores mock <em>form validation techniques</em> with
-            features such as being able to generate a randomized custom ticket
-            in similar format to a gift or CC. The form may only be submitted if
-            the proper mock data is supplied to the appropriate fields.
-          </p>
-        </>
-      ),
-      demoUrl: "https://authentix.netlify.app/",
-      image: authySite,
-    },
-    {
-      name: "Random Website Generator",
-      techstack: (
-        <>
-          {htmlLogo}
-          {cssLogo}
-          {jsLogo}
-        </>
-      ),
-      dependencies: <>{netlifyLogo}</>,
-      description: (
-        <>
-          <p>
-            This website is powered by HTML, CSS, JavaScript and{" "}
-            <em>deployed with Netlify</em>. This tool immerses users in a
-            recreation of the Windows XP operating system, complete with a
-            pseudo Microsoft Paint allowing users to create digital artworks and
-            save them locally, a pseudo Internet Explorer that serves as a
-            random website generator allowing users to visit over 100 websites
-            from all corners of the internet, and a live messaging system to
-            chat with other visitors who have visited, this project transports
-            users back in time in a trip down memory lane to relive the charm of
-            the past through a modern lens.
-          </p>
-        </>
-      ),
-      demoUrl: "https://thedesktop.netlify.app",
-      image: webGenerator,
-    },
+    // {
+    //   name: "Ticket Authentication Site",
+    //   techstack: (
+    //     <>
+    //       {viteLogo}
+    //       {reactLogo}
+    //       {jsLogo}
+    //       {sassLogo}
+    //     </>
+    //   ),
+    //   dependencies: (
+    //     <>
+    //       {netlifyLogo}
+    //       <p>React Context</p>
+    //     </>
+    //   ),
+    //   description: (
+    //     <>
+    //       <p>
+    //         This website explores mock <em>form validation techniques</em> with
+    //         features such as being able to generate a randomized custom ticket
+    //         in similar format to a gift or CC. The form may only be submitted if
+    //         the proper mock data is supplied to the appropriate fields.
+    //       </p>
+    //     </>
+    //   ),
+    //   demoUrl: "https://authentix.netlify.app/",
+    //   image: authySite,
+    // },
+    // {
+    //   name: "Random Website Generator",
+    //   techstack: (
+    //     <>
+    //       {htmlLogo}
+    //       {cssLogo}
+    //       {jsLogo}
+    //     </>
+    //   ),
+    //   dependencies: <>{netlifyLogo}</>,
+    //   description: (
+    //     <>
+    //       <p>
+    //         This website is powered by HTML, CSS, JavaScript and{" "}
+    //         <em>deployed with Netlify</em>. This tool immerses users in a
+    //         recreation of the Windows XP operating system, complete with a
+    //         pseudo Microsoft Paint allowing users to create digital artworks and
+    //         save them locally, a pseudo Internet Explorer that serves as a
+    //         random website generator allowing users to visit over 100 websites
+    //         from all corners of the internet, and a live messaging system to
+    //         chat with other visitors who have visited, this project transports
+    //         users back in time in a trip down memory lane to relive the charm of
+    //         the past through a modern lens.
+    //       </p>
+    //     </>
+    //   ),
+    //   demoUrl: "https://thedesktop.netlify.app",
+    //   image: webGenerator,
+    // },
   ];
 
   return (
